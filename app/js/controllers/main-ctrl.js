0783bf6dbb5fdfedc5f48213c945f7d6bb87ba59
Jake Marsh <jakemmarsh@gmail.com>
2013-09-07 12:40:06 -0400

    incorporated angular-file-upload

--- app/js/controllers/main-ctrl.js
+++ app/js/controllers/main-ctrl.js
@@ -2,15 +2,29 @@ define(['./index'], function (controllers) {
     'use strict';
     controllers.controller('mainCtrl', function ($scope, File) {
     	$scope.fileUploaded = false;
+    	$scope.fileSelected = false;
+    	var $file;
 
-    	$scope.uploadFile = function() {
-    		File.upload($scope.fileToUpload).then(function (data) {
-				console.log(data);
-				$scope.fileUploaded = true;
-		    },
-		    function (errorMessage) {
-		        console.log(errorMessage);
-		    });
-    	}
+    	$scope.onFileSelect = function($files) {
+    		// store uploaded file
+		    for (var i = 0; i < $files.length; i++) {
+		      $file = $files[i];
+		    }
+		    $scope.fileSelected = true;
+		}
+
+		$scope.uploadFile = function() {
+			// verify that a file has been selected
+			if($file) {
+				// make HTTP call to upload $file
+				File.upload($file).then(function (data) {
+					console.log(data);
+			        $scope.fileUploaded = true;
+			    },
+			    function (errorMessage) {
+			        console.log(errorMessage);
+			    });
+			}
+		}
     });
 });
